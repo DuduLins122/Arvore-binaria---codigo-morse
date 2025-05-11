6c7936c6e09cae69c48824bccd98cd20e6076f0d
DuduLins122 <151662692+DuduLins122@users.noreply.github.com>
2025-05-11 15:22:36 -0300

    Update Principal.java

--- ArvoreMorse/Principal.java
+++ ArvoreMorse/Principal.java
@@ -3,13 +3,13 @@ public class Principal {
         ArvoreBinariaMorse arvore = new ArvoreBinariaMorse();
         arvore.inicializar();
 
-        // Inserção de exemplo
+        
         arvore.inserir("...", 'S');
         arvore.inserir("---", 'O');
 
-        // Testes
-        System.out.println(arvore.buscar("...")); // Deve imprimir 'S'
-        System.out.println(arvore.buscar("---")); // Deve imprimir 'O'
-        System.out.println(arvore.decodificarFrase("... --- ...")); // Deve imprimir 'SOS'
+        
+        System.out.println(arvore.buscar("...")); 
+        System.out.println(arvore.buscar("---")); 
+        System.out.println(arvore.decodificarFrase("... --- ...")); 
     }
 }
